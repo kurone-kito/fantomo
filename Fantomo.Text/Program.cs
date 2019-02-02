982f874df1c64dac309f635b9521f4d0859b193e
kurone-kito <krone@kit.black>
2019-02-02 18:09:29 +0900

    Added the empty project

---
+++ Fantomo.Text/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Fantomo.Text
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
