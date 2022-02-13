846b2b51cb0706c31d069f1938d5db1732f8a0fa
kurone-kito <krone@kit.black>
2022-02-14 00:19:37 +0900

    feat: added the logic that toggles the door or wall in the game field

---
+++ Assets/Scripts/Room.cs
@@ -0,0 +1,65 @@
+﻿using UdonSharp;
+using UnityEngine;
+
+/// <summary>部屋制御用ロジック。</summary>
+[UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
+public class Room : UdonSharpBehaviour
+{
+    /// <value>-X側のドアが存在するかどうか。</value>
+    [UdonSynced]
+    public bool existsDoorNX = false;
+    /// <value>+X側のドアが存在するかどうか。</value>
+    [UdonSynced]
+    public bool existsDoorPX = false;
+    /// <value>-Z側のドアが存在するかどうか。</value>
+    [UdonSynced]
+    public bool existsDoorNZ = false;
+    /// <value>+Z側のドアが存在するかどうか。</value>
+    [UdonSynced]
+    public bool existsDoorPZ = false;
+    /// <value>-X側のドアと周囲のオブジェクトを含むコンテナ。</value>
+    public GameObject doorContainerNX = null;
+    /// <value>+X側のドアと周囲のオブジェクトを含むコンテナ。</value>
+    public GameObject doorContainerPX = null;
+    /// <value>-Z側のドアと周囲のオブジェクトを含むコンテナ。</value>
+    public GameObject doorContainerNZ = null;
+    /// <value>+Z側のドアと周囲のオブジェクトを含むコンテナ。</value>
+    public GameObject doorContainerPZ = null;
+    /// <value>-X側の壁オブジェクトを含むコンテナ。</value>
+    public GameObject wallContainerNX = null;
+    /// <value>+X側の壁オブジェクトを含むコンテナ。</value>
+    public GameObject wallContainerPX = null;
+    /// <value>-Z側の壁オブジェクトを含むコンテナ。</value>
+    public GameObject wallContainerNZ = null;
+    /// <value>+Z側の壁オブジェクトを含むコンテナ。</value>
+    public GameObject wallContainerPZ = null;
+
+    /// <summary>
+    /// 同期データを受領・適用した後に呼び出す、コールバック。
+    /// </summary>
+    public override void OnDeserialization()
+    {
+        this.updateVisible();
+    }
+
+    /// <summary>
+    /// このコンポーネント初期化時に呼び出す、コールバック。
+    /// </summary>
+    void Start()
+    {
+        this.updateVisible();
+    }
+
+    /// <summary>表示状態を更新します。</summary>
+    private void updateVisible()
+    {
+        this.doorContainerNX.SetActive(this.existsDoorNX);
+        this.doorContainerPX.SetActive(this.existsDoorPX);
+        this.doorContainerNZ.SetActive(this.existsDoorNZ);
+        this.doorContainerPZ.SetActive(this.existsDoorPZ);
+        this.wallContainerNX.SetActive(!this.existsDoorNX);
+        this.wallContainerPX.SetActive(!this.existsDoorPX);
+        this.wallContainerNZ.SetActive(!this.existsDoorNZ);
+        this.wallContainerPZ.SetActive(!this.existsDoorPZ);
+    }
+}
