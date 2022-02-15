34a997e16d4efecf8c16c116e2acc70742888d58
kurone-kito <krone@kit.black>
2022-02-16 01:57:48 +0900

    feat: added a door switch

---
+++ Assets/Scripts/DoorSwitch.cs
@@ -0,0 +1,18 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+
+public class DoorSwitch : UdonSharpBehaviour
+{
+    public GameObject door = null;
+    void Start()
+    {
+
+    }
+
+    public override void Interact()
+    {
+        Debug.Log("Interact");
+        // door.GetComponent<Animator>().enabled = !door.GetComponent<Animator>().enabled;
+    }
+}
