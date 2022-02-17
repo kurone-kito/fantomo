6fb8d9924ac7030b917dff8f396c4d098c8efe18
kurone-kito <krone@kit.black>
2022-02-18 06:40:00 +0900

    feat: added the light for wall

---
+++ Assets/Scripts/Wall.cs
@@ -0,0 +1,27 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+
+/// <summary>壁機能のロジック。</summary>
+[UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+public class Wall : UdonSharpBehaviour
+{
+    /// <value>開始時に壁の位置をずらす定数。</value>
+    private readonly Vector3 wallMove = new Vector3(0, 3.5f, 0);
+
+    /// <summary>
+    /// <para>
+    /// このコンポーネントが初期化された際に呼び出される、コールバック。
+    /// </para>
+    /// <para>
+    /// ベイクの不整合を抑止するため、別の位置でベイクした後、
+    /// 動的に座標移動をしています。
+    /// </para>
+    /// </summary>
+    void Start()
+    {
+        // これだけの処理なので、Udon graph で十分でしょうと思っていましたが、
+        // transform.position に加算するだけなのに、詰みました。。
+        transform.position += wallMove;
+    }
+}
