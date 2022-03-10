5494f58676eded4920f1e50afbb8d0eaf7b0cb00
kurone-kito <krone@kit.black>
2022-03-10 23:51:48 +0900

    feat: implemented the dynamic height door switch

--- Assets/Scripts/LockableArea.cs
+++ Assets/Scripts/LockableArea.cs
@@ -9,15 +9,46 @@ public class LockableArea : UdonSharpBehaviour
     /// <summary>ドア本体オブジェクト。</summary>
     public Door door = null;
 
+    /// <summary>プレイヤーが当該エリアに存在するかどうか。</summary>
+    public bool isLocalPlayerExists
+    {
+        get;
+        private set;
+    }
+
+    /// <summary>
+    /// プレイヤーが当該エリアに侵入した時に呼び出す、コールバック。
+    /// </summary>
+    /// <param name="player">対象プレイヤー オブジェクト。</param>
+    public override void OnPlayerTriggerEnter(VRCPlayerApi player)
+    {
+        if (player.isLocal)
+        {
+            this.isLocalPlayerExists = true;
+        }
+    }
+
     /// <summary>
     /// プレイヤーが当該エリアから離れた時に呼び出す、コールバック。
     /// </summary>
     /// <param name="player">対象プレイヤー オブジェクト。</param>
     public override void OnPlayerTriggerExit(VRCPlayerApi player)
     {
-        if (player.isLocal && this.door != null)
+        if (player.isLocal)
         {
-            this.door.CancelLock();
+            this.isLocalPlayerExists = false;
+            if (this.door != null)
+            {
+                this.door.CancelLock();
+            }
         }
+    }
+
+    /// <summary>
+    /// このコンポーネント初期化時に呼び出す、コールバック。
+    /// </summary>
+    void Start()
+    {
+        this.isLocalPlayerExists = false;
     }
 }
---
+++ Assets/Scripts/SwitchContainer.cs
@@ -0,0 +1,32 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+
+/// <summary>ドア開放・施錠スイッチにおける、コンテナのロジック。</summary>
+[UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+public class SwitchContainer : UdonSharpBehaviour
+{
+    /// <summary>施錠可能エリア オブジェクト。</summary>
+    public LockableArea lockableArea = null;
+
+    /// <summary>
+    /// オブジェクトの動的調整時における、Y軸のオフセット座標。
+    /// </summary>
+    public float positionOffsetY = 0.0f;
+
+    /// <summary>毎フレーム呼び出される、コールバック。</summary>
+    void Update()
+    {
+        if (this.lockableArea != null && this.lockableArea.isLocalPlayerExists)
+        {
+            var trackingData =
+                Networking.LocalPlayer.GetTrackingData(
+                    VRCPlayerApi.TrackingDataType.Head);
+            transform.position = new Vector3(
+                transform.position.x,
+                trackingData.position.y + positionOffsetY,
+                transform.position.z);
+        }
+    }
+}
