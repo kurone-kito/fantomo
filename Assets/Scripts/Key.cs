5d44cf61d2d7971b7187265221179cbdf6e4925d
kurone-kito <krone@kit.black>
2022-03-20 08:00:01 +0900

    feat: added a key prefab

---
+++ Assets/Scripts/Key.cs
@@ -0,0 +1,15 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+
+/// <summary>鍵制御用ロジック。</summary>
+[UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+public class Key : UdonSharpBehaviour
+{
+    /// <value>取得判定。</value>
+    [SerializeField]
+    private Collider body = null;
+    /// <value>吸引判定。</value>
+    [SerializeField]
+    private Collider suction = null;
+}
