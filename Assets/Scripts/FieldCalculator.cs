00aa4b61269b39b2642772a35246124e7db4af33
kurone-kito <krone@kit.black>
2022-05-05 13:55:49 +0900

    feat: added the calculator for game field mapping

---
+++ Assets/Scripts/FieldCalculator.cs
@@ -0,0 +1,12 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+
+/// <summary>フィールド算出のロジック。</summary>
+[UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+public class FieldCalculator : UdonSharpBehaviour
+{
+    /// <value>同期管理オブジェクト。</value>
+    [SerializeField]
+    private SyncManager syncManager;
+}
