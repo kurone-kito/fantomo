17a67f350ddc15eb45892004f5aa2fe60c9b7ea4
kurone-kito <krone@kit.black>
2022-05-17 02:07:02 +0900

    feat: improved the constants

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -6,100 +6,87 @@ using UdonSharp;
 public class Constants : UdonSharpBehaviour
 {
     /// <summary>-Y の方角を示す定数。</summary>
-    private const int _DIR_N = 0;
-
-    /// <summary>-Y の方角を示す定数。</summary>
-    public int DIR_N => _DIR_N;
-
-    /// <summary>+X の方角を示す定数。</summary>
-    private const int _DIR_E = 1;
-
-    /// <summary>+X の方角を示す定数。</summary>
-    public int DIR_E => _DIR_E;
-
-    /// <summary>-X の方角を示す定数。</summary>
-    private const int _DIR_W = 2;
-
-    /// <summary>-X の方角を示す定数。</summary>
-    public int DIR_W => _DIR_W;
+    public int DIR_N => 0;
 
     /// <summary>+Y の方角を示す定数。</summary>
-    private const int _DIR_S = 3;
+    public int DIR_S => 1;
 
-    /// <summary>+Y の方角を示す定数。</summary>
-    public int DIR_S => _DIR_S;
+    /// <summary>-X の方角を示す定数。</summary>
+    public int DIR_W => 2;
+
+    /// <summary>+X の方角を示す定数。</summary>
+    public int DIR_E => 3;
 
     /// <summary>方角を示す定数の最大値。</summary>
-    private const int _DIR_MAX = 4;
-
-    /// <summary>方角を示す定数の最大値。</summary>
-    public int DIR_MAX => _DIR_MAX;
+    public int DIR_MAX => 4;
 
     /// <summary>解錠状態を示すフラグ定数。</summary>
-    private const int _LOCKSTATE_UNLOCKED = 0;
-
-    /// <summary>解錠状態を示すフラグ定数。</summary>
-    public int LOCKSTATE_UNLOCKED => _LOCKSTATE_UNLOCKED;
+    public int LOCKSTATE_UNLOCKED => 0;
 
     /// <summary>自プレイヤーによる施錠状態を示すフラグ定数。</summary>
-    private const int _LOCKSTATE_LOCKED_BY_ME = 1;
-
-    /// <summary>自プレイヤーによる施錠状態を示すフラグ定数。</summary>
-    public int LOCKSTATE_LOCKED_BY_ME => _LOCKSTATE_LOCKED_BY_ME;
+    public int LOCKSTATE_LOCKED_BY_ME => 1;
 
     /// <summary>他プレイヤーによる施錠状態を示すフラグ定数。</summary>
-    private const int _LOCKSTATE_LOCKED_BY_EMEMY = 2;
-
-    /// <summary>他プレイヤーによる施錠状態を示すフラグ定数。</summary>
-    public int LOCKSTATE_LOCKED_BY_EMEMY => _LOCKSTATE_LOCKED_BY_EMEMY;
+    public int LOCKSTATE_LOCKED_BY_EMEMY => 2;
 
     /// <summary>施錠状態の固定を示すフラグ定数。</summary>
-    private const int _LOCKSTATE_DISABLED = 4;
-
-    /// <summary>施錠状態の固定を示すフラグ定数。</summary>
-    public int LOCKSTATE_DISABLED => _LOCKSTATE_DISABLED;
+    public int LOCKSTATE_DISABLED => 4;
 
     /// <summary>鍵の設置数。</summary>
-    private const int _NUM_KEYS = 9;
-
-    /// <summary>鍵の設置数。</summary>
-    public int NUM_KEYS => _NUM_KEYS;
+    public int NUM_KEYS => 9;
 
     /// <summary>地雷の設置数。</summary>
-    private const int _NUM_MINES = 9;
-
-    /// <summary>地雷の設置数。</summary>
-    public int NUM_MINES => _NUM_MINES;
+    public int NUM_MINES => 9;
 
     /// <summary>最大エントリー可能数。</summary>
-    private const int _NUM_PLAYERS = 3;
+    public int NUM_PLAYERS => 3;
 
-    /// <summary>最大エントリー可能数。</summary>
-    public int NUM_PLAYERS => _NUM_PLAYERS;
+    /// <summary>部屋フラグにおける、-Y の方角を示すビット。</summary>
+    public byte ROOM_FLG_DIR_N => (byte)(1 << 0);
+
+    /// <summary>部屋フラグにおける、+Y の方角を示すビット。</summary>
+    public byte ROOM_FLG_DIR_S => (byte)(1 << 1);
+
+    /// <summary>部屋フラグにおける、-X の方角を示すビット。</summary>
+    public byte ROOM_FLG_DIR_W => (byte)(1 << 2);
+
+    /// <summary>部屋フラグにおける、+X の方角を示すビット。</summary>
+    public byte ROOM_FLG_DIR_E => (byte)(1 << 3);
+
+    /// <summary>部屋フラグにおける、全方位を示すビット。</summary>
+    public byte ROOM_FLG_DIR_ALL => (byte)(ROOM_FLG_DIR_E | ROOM_FLG_DIR_W | ROOM_FLG_DIR_S | ROOM_FLG_DIR_N);
+
+    /// <summary>部屋フラグにおける、鍵所持を示すビット。</summary>
+    public byte ROOM_FLG_HAS_KEY => (byte)(1 << 4);
+
+    /// <summary>部屋フラグにおける、地雷所持を示すビット。</summary>
+    public byte ROOM_FLG_HAS_MINE => (byte)(1 << 5);
+
+    /// <summary>
+    /// 部屋フラグにおける、スポーン地点所持を示すビット。
+    /// </summary>
+    public byte ROOM_FLG_HAS_SPAWN => (byte)(1 << 6);
+
+    /// <summary>部屋フラグにおける、ビット数。</summary>
+    public byte ROOM_BIT_MAX => 8;
 
     /// <summary>ゲームフィールドの X 軸における部屋数。</summary>
-    private const int _ROOMS_WIDTH = 8;
-
-    /// <summary>ゲームフィールドの X 軸における部屋数。</summary>
-    public int ROOMS_WIDTH => _ROOMS_WIDTH;
-
-    /// <summary>扉を削除して、袋小路を形成する比率。</summary>
-    private const float _ROOM_REMOVE_DOOR_RATE = 0.01f;
+    /// <remarks>
+    /// ビット演算の関係上、この値は 11 以下である必要があります。
+    /// </remarks>
+    public int ROOMS_WIDTH => 8;
 
     /// <summary>扉を削除して、袋小路を形成する比率。</summary>
-    public float ROOM_REMOVE_DOOR_RATE => _ROOM_REMOVE_DOOR_RATE;
+    public float ROOM_REMOVE_DOOR_RATE => 0.01f;
 
     /// <summary>部屋の大きさ。</summary>
-    private const float _ROOM_SIZE = 10f;
-
-    /// <summary>部屋の大きさ。</summary>
-    public float ROOM_SIZE => _ROOM_SIZE;
+    public float ROOM_SIZE => 10f;
 
     /// <summary>ゲームフィールドにおける部屋数。</summary>
-    private const int _NUM_ROOMS = _ROOMS_WIDTH * _ROOMS_WIDTH;
-
-    /// <summary>ゲームフィールドにおける部屋数。</summary>
-    public int NUM_ROOMS => _NUM_ROOMS;
+    /// <remarks>
+    /// ビット演算の関係上、この値は 64 以下である必要があります。
+    /// </remarks>
+    public int NUM_ROOMS => ROOMS_WIDTH * ROOMS_WIDTH;
 
     /// <summary>
     /// <seealso cref="UdonSharpBehaviour.VRCInstantiate"/>する、
@@ -108,11 +95,5 @@ public class Constants : UdonSharpBehaviour
     /// <remarks>
     /// 末尾の 2 は、MirrorSystem と EntrySystem を指している。
     /// </remarks>
-    private const int _NUM_INSTANTIATES = _NUM_KEYS + _NUM_MINES + _NUM_ROOMS + 2;
-
-    /// <summary>
-    /// <seealso cref="UdonSharpBehaviour.VRCInstantiate"/>する、
-    /// オブジェクト一覧の要素数。
-    /// </summary>
-    public int NUM_INSTANTIATES => _NUM_INSTANTIATES;
+    public int NUM_INSTANTIATES => NUM_KEYS + NUM_MINES + NUM_ROOMS + 2;
 }
